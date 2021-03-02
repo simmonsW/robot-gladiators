223e2357a6028fdaaecc7dca17b8e572f1a4eb26
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-01 16:05:20 -0800

    fixed playerName bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,17 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var getPlayerName = function() {
+  var name="";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?")
+  }
+
+  console.log("Your robot's name is " + name);
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
